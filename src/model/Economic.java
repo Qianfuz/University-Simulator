0c535ed111d488a4612338b2e06d9bb0d09239af
Qianfuz <737280879@qq.com>
2025-12-14 11:49:49 +0800

    V0.1.3 加入银行

---
+++ src/model/Economic.java
@@ -0,0 +1,62 @@
+package model;
+
+import model.Student;
+
+import java.util.*;
+
+public class Economic {
+    String name;
+    String introduce;
+    int price;
+    int rate;
+    int  probably;
+
+    public Economic(String name,String introduce,int price,int rate,int  probably){
+        this.introduce=introduce;
+        this.name=name;
+        this.price=price;
+        this.rate=rate;
+        this.probably=probably;
+
+    }
+
+    public void setIntroduce(String introduce) {
+        this.introduce = introduce;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getIntroduce() {
+        return introduce;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getProbably() {
+        return probably;
+    }
+
+    public void setProbably(int probably) {
+        this.probably = probably;
+    }
+}
